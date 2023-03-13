8ff50db1713141fb210735a7fb77e498cd889466
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-13 12:19:45 +0200

    Lists of random int

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int minValue = 0;
+        int maxValue = 100;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 25; i++){
+            int randInt = ThreadLocalRandom.current().nextInt(minValue, maxValue);
+            list.add(randInt);
+        }
+
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
     }
 }
